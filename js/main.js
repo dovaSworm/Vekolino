3169691ed2e7862453ac9e481cd499b9b351a0aa
Danijel Doveden <danijeldoveden.55@gmail.com>
2019-10-15 00:14:07 +0200

    animated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function() {
-    $('.carousel').carousel();
 
     if ($(window).width() < 1024) {
         // $("a.navbar-brand").removeClass("ml-4");
@@ -32,7 +31,45 @@ $(function() {
     }
     var date = new Date();
     var year = date.getFullYear();
-    console.log(year);
+    // console.log(year);
     document.getElementById('prava').innerHTML = "Copyright &copy; RDdesign " + year + ". All Rights Reserved";
 
 });
+
+
+window.onscroll = function() {
+    myFunction()
+};
+
+var windowHeight = (window.innerHeight * 3) / 10;
+var about = document.getElementById("about");
+var services = document.getElementById("serviceshref");
+var team = document.getElementById("team");
+var contact = document.getElementById("contact");
+
+function myFunction() {
+    var positionS = services.getBoundingClientRect().top;
+    var positionA = about.getBoundingClientRect().top;
+    var positionT = team.getBoundingClientRect().top;
+    var positionC = contact.getBoundingClientRect().top;
+
+    if (document.documentElement.scrollTop >= windowHeight) {
+        about.getElementsByClassName("col")[0].classList.add("animaimg");
+        about.getElementsByClassName("col")[1].classList.add("animaright");
+    }
+    if (windowHeight >= positionS) {
+        services.getElementsByClassName("col")[0].classList.add("animaleft");
+        services.getElementsByClassName("col")[1].classList.add("animaimg");
+    }
+    if (windowHeight >= positionT) {
+        team.getElementsByClassName("col")[0].classList.add("animaright");
+        team.getElementsByClassName("col")[1].classList.add("animaright");
+        team.getElementsByClassName("col")[2].classList.add("animaright");
+        team.getElementsByClassName("col")[3].classList.add("animaright");
+    }
+    if (windowHeight >= positionC) {
+        contact.getElementsByClassName("container")[0].classList.add("animaimg");
+        // contact.getElementsByClassName("col")[1].classList.add("animaright");
+    }
+    console.log(positionS);
+}
